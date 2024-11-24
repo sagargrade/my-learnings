eccb77e3aa9c2ae478f030c834b29f55a8b8534a
Sagar <sagargrade@yahoo.com>
2024-11-24 13:21:58 +0530

    Java - Algorithm : Binary Search - Iterative and Recursive

--- programming-languages/java/src/main/java/edu/learning/algorithms/search/BinarySearch.java
+++ programming-languages/java/src/main/java/edu/learning/algorithms/search/BinarySearch.java
@@ -2,7 +2,23 @@ package edu.learning.algorithms.search;
 
 public class BinarySearch {
 
-    public int search(int[] array, int value, int startIndex, int endIndex) {
+    public int searchIterative(int[] array, int value) {
+        int startIndex = 0;
+        int endIndex = array.length - 1;
+        while (startIndex <= endIndex) {
+            int mid = (startIndex + endIndex) / 2;
+            if (array[mid] == value) {
+                return mid;
+            } else if (array[mid] > value) {
+                endIndex = mid - 1;
+            } else {
+                startIndex = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int searchRecursive(int[] array, int value, int startIndex, int endIndex) {
         if (startIndex > endIndex) {
             return -1;
         }
@@ -10,9 +26,9 @@ public class BinarySearch {
         if (array[mid] == value) {
             return mid;
         } else if (array[mid] > value) {
-            return search(array, value, startIndex, mid);
+            return searchRecursive(array, value, startIndex, mid - 1);
         } else {
-            return search(array, value, mid + 1, endIndex);
+            return searchRecursive(array, value, mid + 1, endIndex);
         }
     }
 }
--- programming-languages/java/src/test/java/edu/learning/algorithms/search/BinarySearchTest.java
+++ programming-languages/java/src/test/java/edu/learning/algorithms/search/BinarySearchTest.java
@@ -1,11 +1,16 @@
 package edu.learning.algorithms.search;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class BinarySearchTest {
@@ -13,14 +18,36 @@ class BinarySearchTest {
     @InjectMocks
     private BinarySearch binarySearch;
 
-    @Test
-    void search() {
+    static Stream<Arguments> provideSearchData() {
+        return Stream.of(
+                Arguments.of(new int[]{10, 20, 30, 40, 50, 60}, 20, 1),
+                Arguments.of(new int[]{5, 15, 25}, 25, 2),
+                Arguments.of(new int[]{5, 10, 15, 25, 35}, 20, -1),
+                Arguments.of(new int[]{10, 15}, 20, -1),
+                Arguments.of(new int[]{10, 15}, 5, -1),
+                Arguments.of(new int[]{10, 10}, 10, 0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideSearchData")
+    @DisplayName("Should return index if found else -1 in iterative way")
+    void testSearchIterative(int[] array, int value, int expectedIndex) {
         //arrange
-        int[] array = {10, 20, 30, 40, 50, 60};
-        int value = 20;
         //act
-        int searchIndex = binarySearch.search(array, value, 0, array.length - 1);
+        int searchIndex = binarySearch.searchIterative(array, value);
         //assert
-        assertEquals(1, searchIndex);
+        assertEquals(expectedIndex, searchIndex);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideSearchData")
+    @DisplayName("Should return index if found else -1 in recursive way")
+    void testSearchRecursive(int[] array, int value, int expectedIndex) {
+        //arrange
+        //act
+        int searchIndex = binarySearch.searchRecursive(array, value, 0, array.length - 1);
+        //assert
+        assertEquals(expectedIndex, searchIndex);
     }
 }
